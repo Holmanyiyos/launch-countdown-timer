4ea1711fe2699489847607ef8fc116dd966ea91f
Holman Plazas <holmanyiyos@gmail.com>
2022-05-24 09:51:14 -0500

    inicio modal errores y final

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import './App.css';
+import ReactDOM  from 'react-dom';
 import React, { useState, useEffect } from "react";
 import Countdown from "./pages/countDown/Countdown"
 import Inputview from './pages/InputView/InputView';
 import { subtractDate } from './utilities/getTime.utilities';
 import Footer from './components/Footer';
+import ModalPortal from './components/Modal';
 
 function App() {
   const [isActive, setIsActive] = useState(false)
   const [timeSelected, setTimeSelected] = useState("");
   const [time, setTime] = useState("");
   const [start, setStart] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-  
   useEffect(()=>{
-      const getTime = subtractDate(timeSelected)
-      if(getTime != null){
-        if(isActive){
-            setTime(getTime)
-        }
-      }else{
-        setIsActive(false)
-        setTime("")
-      }
+    const getTime = subtractDate(timeSelected)
+    if(getTime != null){
+    if(isActive){
+        setTime(getTime)
+    }
+    }else{
+      setIsActive(false)
+      setTime("")
+      setModalOpen(true)
+    }
   }, [start])
   
   if (isActive) {
@@ -31,18 +34,19 @@ function App() {
       }, 1000)
   }
 
-  
+  function handleDateSelected(e){
+    setTimeSelected(e.target.value)
+  }
 
-    function handleDateSelected(e){
-       setTimeSelected(e.target.value)
-    }
-
-    function handleClick(){
-      setIsActive(true)
+  function handleClick(){
+    setIsActive(true)
   }
 
   return (
     <div className="App">
+      {modalOpen && ReactDOM.createPortal(
+      <ModalPortal setModalOpen={setModalOpen}/>, 
+      document.querySelector("#modal"))}
       <section className='main__section'>
         <Inputview handleDateSelected= {handleDateSelected} handleClick={handleClick}/>
         <p>WE'RE LAUNCHING SOON</p>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,27 @@
-import ReactDOM from "react-dom";
+import React ,{useState, useEffect} from "react";
+import "./styles.css"
 
-const ModalPortal = ()=>{
-    return ReactDOM.createPortal(
-        <div>
-            <h3>Portal</h3>
+
+const ModalPortal = (props)=>{
+    const response = {messagge: "Terminaste la cuenta atrás"};
+    const [resp, setResp] = useState("")
+    const removeModal =()=>{
+        props.setModalOpen(false)
+    }
+    useEffect(()=>{
+        if(response.messagge){
+            try {
+                setResp(response.messagge)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    },[])
+    
+    return (
+        <div className="modal">
+            <h3>{resp}</h3>
+            <span onClick={removeModal}>✖</span>
         </div>
     )
 };
